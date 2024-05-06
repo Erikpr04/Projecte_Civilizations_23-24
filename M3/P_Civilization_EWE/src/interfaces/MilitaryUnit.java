055521b968c74e533ebca2c3bc731e900fc1d729
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-05-06 17:46:16 +0200

    Interfaz MIlitaryUnit hecha

--- M3/P_Civilization_EWE/src/interfaces/MilitaryUnit.java
+++ M3/P_Civilization_EWE/src/interfaces/MilitaryUnit.java
@@ -1,5 +1,39 @@
 package interfaces;
 
 public interface MilitaryUnit extends Variable {
-
+	//Devuelve el ataque de la unidad.
+	abstract int attack(); 
+	
+	//Resta el daño introducido al blindaje.
+	abstract void takeDamage(int recievedDamage); 
+	
+	//Devuelve la armadura que tengamos actualmente después de recibir golpe.
+	abstract int getActualArmor(); 
+	
+	//Devuelve el coste de comida que tiene crear la unidad.
+	abstract int getFoodCost(); 
+	
+	//Devuelve el coste de madera que tiene crear la unidad.
+	abstract int getWoodCost(); 
+	
+	//Devuelve el coste de hierro que tiene crear la unidad.
+	abstract int getIronCost(); 
+	
+	//Devuelve el coste de mana que tiene crear la unidad.
+	abstract int getManaCost(); 
+	
+	//Devuelve la probabilidad de generar residuos cuando la armadura sea menor o igual a 0.
+	abstract int getChanceGeneratingWaste(); 
+	
+	//Devuelve la probabilidad de volver a atacar.
+	abstract int getChanceAttackAgain(); 
+	
+	//Resetea la armadura.
+	abstract void resetArmor(); 
+	
+	//Establecer experienca al valor introducido.
+	abstract void setExperience(int n); 
+	
+	//Devuelve experiencia de la unidad.
+	abstract void getExperience(); 
 }
