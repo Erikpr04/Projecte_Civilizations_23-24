15d520135162f239d187387577286ac39e474fdb
ErikRojaas <erojasaguilar.cf@iesesteveterradas.cat>
2024-05-25 12:15:12 +0200

    Penultimos retoques a la página
    he quitado el archivo de dataToXml porque era para la base de datos, y he añadido la fuente.

--- M4/xmlToHtml.py
+++ M4/xmlToHtml.py
@@ -29,6 +29,39 @@ def transform_unit(xmlTree, name):
     write_html(f"./html/{name}.html", htmlResult)
 
 
+# Crear un índice
+def transform_index_receptes():
+    # Crear el árbol XSL para el índice
+    xslIndex = read_xml('./xml/template.xsl')
+    xslTreeIndex = etree.XML(xslIndex)
+
+    # Leer y combinar los archivos XML especificados
+    xml_files = [
+        './xml/attack_units.xml',
+        './xml/defences.xml',
+        './xml/special_units.xml',
+        './xml/buildings.xml'
+    ]
+
+    combined_root = etree.Element("root")
+
+    for xml_file in xml_files:
+        xml_data = read_xml(xml_file)
+        xml_tree = etree.XML(xml_data)
+        combined_root.append(xml_tree)
+
+    combined_tree = etree.ElementTree(combined_root)
+
+    # Transformar el árbol combinado según el template XSLT
+    transform = etree.XSLT(xslTreeIndex)
+    htmlDom = transform(combined_tree)
+    htmlResult = etree.tostring(htmlDom, pretty_print=True).decode('utf-8')
+    
+    # Escribir el resultado HTML
+    write_html("./html/index.html", htmlResult)
+
+
+
 # Borrar los archivos .html creados anteriormente
 for file in os.listdir("./html/"):
     if file.endswith(".html"):
@@ -75,3 +108,7 @@ xmlTree = etree.XML(xml)
 for unit in xmlTree.findall('building'):
     unit_name = unit.findtext('name')
     transform_unit(xmlTree, unit_name)
+
+
+# Generar el índice HTML
+transform_index_receptes()
